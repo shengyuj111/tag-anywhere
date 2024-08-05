e1e7f4a3319407fa727657fb1e77edd7fc523892
shengyuj111 <shengyujiangpersonal@gmail.com>
2024-08-05 21:32:23 +1000

    Add refresh button and remove transition in mode toggle button

---
+++ src/components/composition/refresh-button.tsx
@@ -0,0 +1,15 @@
+import { RefreshCwIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import { useAppDispatch } from "@/store/hooks";
+import apiSlice from "@/api/api-slice";
+
+
+export const RefreshButton = () => {
+    const dispatch = useAppDispatch();
+    return (
+        <Button variant="outline" size="icon" onClick={() => dispatch(apiSlice.util.resetApiState())}>
+          <RefreshCwIcon className="h-[1.2rem] w-[1.2rem]" />
+          <span className="sr-only">Refresh</span>
+        </Button>
+    );
+}
