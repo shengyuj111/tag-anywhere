d83ca066753a92c4bc47b5f32cc8af0ea657f967
shengyuj111 <shengyujiangpersonal@gmail.com>
2024-08-08 12:57:37 +1000

    Add Debug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,5 +91,16 @@ module.exports = {
         }
       );
     },
+    function ({ addBase }) {
+      addBase({
+        '*': {
+          '-ms-overflow-style': 'none',  /* IE and Edge */
+          'scrollbar-width': 'none',     /* Firefox */
+        },
+        '*::-webkit-scrollbar': {
+          'display': 'none',  /* Chrome, Safari, and Opera */
+        },
+      });
+    },
   ],
 };
